c0d2a6c3d36e4d65a603821390c759b89ebb5c54
Danilo Petrovic <danilo86petrovic@gmail.com>
2018-02-24 16:36:00 +0100

    pocetak 27 lekcije

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,39 @@
 <template>
     <div>
-        <app-header v-on:changeTitleSlanje="updateTitle"></app-header>
-        <app-ninjas v-bind:ninjas_prosledjen="ninjas"></app-ninjas>
-        <app-footer v-bind:prosledi_title="title"></app-footer>
+        <keep-alive>
+            <component v-bind:is="component"></component>
+        </keep-alive>
+        <button v-on:click="component = 'form-one'">Show form one</button>
+        <button v-on:click="component = 'form-two'">Show form two</button>
     </div>
 </template>
-<script>
-    import Header from './components/Header';
-    import Footer from './components/Footer';
-    import Ninjas from './components/Ninjas';
 
-    export default {
-        components: {
-            'app-header': Header,
-            'app-footer': Footer,
-            'app-ninjas': Ninjas
-        },
-        data() {
-            return {
-                title: "title iz app prosledjen propsom u header",
-                ninjas: [
-                    {name: 'Ryu', speciality: 'Vue Components', show: false},
-                    {name: 'Crystal', speciality: 'HTML Wizardy', show: false},
-                    {name: 'Hitoshi', speciality: 'Click Events', show: false},
-                    {name: 'Tango', speciality: 'Conditionals', show: false},
-                    {name: 'Kami', speciality: 'Webpack', show: false},
-                    {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-                ],
-            }
-        },
-        methods: {
-            updateTitle: function(eventKojiSaljemKaoDrugiParametar) {
-                this.title = eventKojiSaljemKaoDrugiParametar;
-            }
+<script>
+// Imports
+import formOne from './components/formOne.vue';
+import formTwo from './components/formTwo.vue';
+
+export default {
+    components: {
+        'form-one': formOne,
+        'form-two': formTwo
+    },
+    data () {
+        return {
+            component: 'form-one'
         }
-    };
+    },
+    methods: {
+        handleSubmit: function(){
+            alert('thanks for submitting');
+        }
+    }
+}
 </script>
+
 <style>
-    body {
-        background-color: lightgray;
-    }
-    h1 {
-        color: blueviolet;
-    }
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-/*definisanje komponente globalno*/
-// import Ninjas from './Ninjas.vue'
-// Vue.component('ninja_component', Ninjas);
-export const bus = new Vue();
-
 new Vue({
-    el: '#app',
-    render: h => h(App)
-});
+  el: '#app',
+  render: h => h(App)
+})
